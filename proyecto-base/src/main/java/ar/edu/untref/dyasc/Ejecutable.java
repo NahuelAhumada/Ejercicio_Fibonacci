f2b456b1130f32fae5e4a15b7e805ba723853252
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-08-19 22:46:55 -0300

    Ejecutable con uso de println

--- proyecto-base/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
+++ proyecto-base/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
@@ -1,7 +1,16 @@
 package ar.edu.untref.dyasc;
 
 public class Ejecutable {
+	
+	
     public static void main(String[] args) {
-        System.out.println("Estoy en ejecución");
+    	if (args.length != 1) {
+            System.out.println("Valor de entrada inavlido - Solo se acepta un numero entero positivo");
+            return;
+        };
+        //Se asume que el dato de entrada en correcto
+        int n  = Integer.parseInt(args[0]);
+        String resultado = Funciones.serieFibonacci(n);
+        System.out.println("fibo<" + n + ">: " + resultado);
     }
 }
